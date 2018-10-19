9bbfbc3b7a03f4b63771949331784fe9012995c5
zhang-xiao-miao <1286874200@qq.com>
2018-10-19 23:12:14 +0800

    dev-server: a case can be used in development enviroment

---
+++ demos/08.development.03.dev-server-simple/src/index.js
@@ -0,0 +1,16 @@
+import './style.css';
+import kido from './cat.jpg';
+;(function() {
+  let oImg = new Image()
+  oImg.className = 'img appended'
+  oImg.src = kido
+  oImg.onload = function() {
+    console.log(this.src)
+  }
+  document.body.appendChild(oImg)
+  console.log('hello, world')
+})()
+
+import './a.js'
+import './b.js'
+import './c.js'
