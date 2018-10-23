3372461b910500622ed07dcec7b038e49ef73956
zhang-xiao-miao <1286874200@qq.com>
2018-10-23 12:19:17 +0800

    caching by contenthash

---
+++ demos/14.caching.01.by-contenthash/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HTMlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    //  the bundle's name now reflects its content (via the hash). Hash name wont change if there is no change in content.
+    filename: 'static/js/[name].[contenthash].js',
+    chunkFilename: 'static/js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  plugins: [
+    new HTMlWebpackPlugin({
+      template: './src/index.html',
+      title: 'webpack caching'
+    })
+  ]
+}
